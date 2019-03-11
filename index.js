a3db12dcf5f130aa31688c524d5754d68da6af70
Frank Harerimana <frankharerimana@Franks-MacBook-Pro.local>
2019-03-11 12:29:47 +0200

    Chore 164529211 adding mocha for tests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('server started successfully');
+})
