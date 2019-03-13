008bc7d2f3c0cb974eac80ff4d8c4a03f55c1ef8
mwibutsa <mflohost@gmail.com>
2019-03-13 13:12:20 +0200

    Format codes according to eslint

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -19,22 +19,22 @@ const sequelize = new Sequelize({
   password: process.env.DBPASSWORD,
   dialect: 'postgres',
   define: {
-    timestamps: false
+    timestamps: false,
   },
   operatorsAliases: false,
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 
 });
 
 // test database connection
 
 sequelize.authenticate()
-.then(() => debugLogger('Connection is established'))
-.catch((error) => debugLogger(`Failled to connect to the database  : ${error}`));
+  .then(() => debugLogger('Connection is established'))
+  .catch(error => debugLogger(`Failled to connect to the database  : ${error}`));
 
 export default sequelize;
