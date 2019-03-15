3cff8a43449e647182265ea46a6731927fe6c99a
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2019-03-15 15:56:41 +0200

    [Feature #164535410 ] comment endpoint

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -37,6 +37,100 @@ class Comment {
       });
     }
   }
+
+  static getAllComments(req, res) {
+    commentModal.findAll()
+      .then((comments) => {
+        if (comments.length === 0) {
+          res.status(404).json({
+            message: 'No comment found',
+          });
+        } else {
+          res.status(200).json({
+            status: '200',
+            data: comments,
+          });
+        }
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+  }
+
+  static postComment(req, res) {
+    const { comment, article, user } = req.body;
+
+    const { error } = joi.validate({
+      comment, article, user,
+    }, validations.createComment);
+
+    if (error) {
+      res.status(400).json({ error: error.details[0].message });
+    } else {
+      commentModal.create({
+        comment,
+        articleid: article,
+        userid: user,
+      })
+        .then((formData) => {
+          res.status(201).json({
+            message: 'Comment posted',
+            data: formData,
+          });
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
+    }
+  }
+
+  static updateComment(req, res) {
+    const { id } = req.params;
+    const { comment, articleid, userid } = req.body;
+    const { error } = joi.validate({
+      id, comment,
+    }, validations.updateSchema);
+
+    if (error) {
+      res.status(400).json({ error: error.details[0].message });
+    } else {
+      commentModal.update({ comment, articleid, userid }, { returning: true, where: { id } })
+        .then((updatedcomment) => {
+          res.status(202).json({
+            data: updatedcomment,
+          });
+        }).catch((err) => {
+          res.status(500).send(err);
+        });
+    }
+  }
+
+  static deleteComment(req, res) {
+    const { id } = req.params;
+    const { error } = joi.validate({
+      id,
+    }, validations.getChema);
+
+    if (error) {
+      res.status(400).json({ error: error.details[0].message });
+    } else {
+      commentModal.destroy({
+        where: {
+          id,
+        },
+      })
+        .then((deletedComment) => {
+          res.status(202).json({
+            message: 'Comment deleted',
+            data: deletedComment,
+          });
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
+    }
+  }
 }
+
 
 export default Comment;
--- src/tests/comment_test.js
+++ src/tests/comment_test.js
@@ -7,6 +7,25 @@ import sequelize from '../config/db-config';
 chai.use(chaiHttp);
 
 describe('Test for comments', () => {
+  before('Create a comment that will be used for test', (done) => {
+    const comment = {
+      comment: 'This article is nice',
+      article: '3',
+      user: '8',
+    };
+    chai.request(index)
+      .post('/comment')
+      .send(comment)
+      .then((res) => {
+        res.body.should.be.a('object');
+        res.body.should.have.property('data');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    done();
+  });
+
   it('It should return comment by their id', (done) => {
     const comment = {
       comment: 'This article is good',
@@ -24,7 +43,43 @@ describe('Test for comments', () => {
         res.body.data.should.have.property('user').eql('7');
       })
       .catch((err) => {
-        // console.log(err);
+        console.log(err);
+      });
+    done();
+  });
+
+  it('It should return comment by their id', (done) => {
+    const comment = {
+      comment: 'This article is good',
+      articleid: 5,
+      userid: 7,
+    };
+    chai.request(index)
+      .get('/comment/1')
+      .send(comment)
+      .then((res) => {
+        res.body.should.be.a('object');
+        res.body.should.have.property('data');
+        res.body.should.have.property('comment').eql('This article is good');
+        res.body.data.should.have.property('article').eql('5');
+        res.body.data.should.have.property('user').eql('7');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    done();
+  });
+
+  it('It should return all comments', (done) => {
+    chai.request(index)
+      .get('/comment')
+      .then((res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.should.have.property('data');
+      })
+      .catch((err) => {
+        console.log(err);
       });
     done();
   });
