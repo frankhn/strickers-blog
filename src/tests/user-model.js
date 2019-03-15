ee9eb92c5e95cad28fcdc8dcd91411589a85485c
mwibutsa <mflohost@gmail.com>
2019-03-15 14:46:18 +0200

    Chore #164630844 Test bad word

--- src/tests/user-model.js
+++ src/tests/user-model.js
@@ -1,18 +1,18 @@
-import User from '../models/user';
 import mocha from 'mocha';
 import chai from 'chai';
 import '@babel/polyfill';
 import debug from 'debug'
-const debugLogger = debug('app:*');
+import User from '../models/user';
+const debugLogger = debug('app:dbtest');
 
 chai.should();
 
 describe('Test User Model', () => {
   it('It should create user model', async (done) => {
-      User.sync({force: true}).then((userModel) => {
-        userModel.should.be.a('object');
-        userModel.should.have.property('rawAttributes');
-      });
-      done();
+    User.sync({ force: true }).then((userModel) => {
+      userModel.should.be.a('object');
+      userModel.should.have.property('rawAttributes');
+    });
+    done();
   });
 });
